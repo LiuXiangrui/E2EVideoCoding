ccc3c3449629c8d3686249eab96e027ede8def14
Xiangrui LIU <xrliu.mail@gmail.com>
2023-02-09 14:43:23 +0800

    init DCVC

--- ModelZoo/DCVC.py
+++ ModelZoo/DCVC.py
@@ -2,27 +2,27 @@ import torch
 import torch.nn as nn
 
 from Modules.PreTransform import IdentityTransform
-from Modules.InterPrediction.MotionEstimation.MotionEstDVC import MotionEstDVC
-from Modules.InterPrediction.MotionCompensation.MotionCompDVC import MotionCompDVC
-from Modules.Codec.FrameCodecDVC import FrameCodecDVC
-from Modules.Codec.MotionCodecDVC import MotionCodecDVC
+from Modules.InterPrediction.MotionEstimation.MotionEstDCVC import MotionEstDCVC
+from Modules.InterPrediction.MotionCompensation.MotionCompDCVC import MotionCompDCVC
+from Modules.Codec.FrameCodecDCVC import FrameCodecDCVC
+from Modules.Codec.MotionCodecDCVC import MotionCodecDCVC
 
 
-class InterFrameCodecDVC(nn.Module):
+class InterFrameCodecDCVC(nn.Module):
     def __init__(self):
         super().__init__()
         self.pre_transform = IdentityTransform()
-        self.motion_est = MotionEstDVC()
-        self.motion_comp = MotionCompDVC()
-        self.frame_codec = FrameCodecDVC()
-        self.motion_codec = MotionCodecDVC()
+        self.motion_est = MotionEstDCVC()
+        self.motion_comp = MotionCompDCVC()
+        self.frame_codec = FrameCodecDCVC()
+        self.motion_codec = MotionCodecDCVC()
 
     def forward(self, cur_frame: torch.Tensor, ref_frame: torch.Tensor):
         cur_frame = self.pre_transform(cur_frame)
         offset = self.motion_est(cur_frame, ref_frame=ref_frame)
         offset_encode_results = self.motion_codec(offset)
-        pred_frame = self.motion_comp(ref_frame, rec_offset=offset_encode_results["rec_offset"])
-        frame_encode_results = self.frame_codec(cur_frame, pred_frame=pred_frame)
+        temporal_ctx = self.motion_comp(ref_frame, rec_offset=offset_encode_results["rec_offset"])
+        frame_encode_results = self.frame_codec(cur_frame, temporal_ctx=temporal_ctx)
         return {
             "rec_offset": offset_encode_results["rec_offset"],
             "rec_frame": frame_encode_results["rec_frame"],
@@ -31,6 +31,6 @@ class InterFrameCodecDVC(nn.Module):
 
 
 if __name__ == "__main__":
-    a = InterFrameCodecDVC()
+    a = InterFrameCodecDCVC()
     b = a(torch.randn(1,3, 128, 128), torch.randn(1, 3, 128, 128))
     print(b)
